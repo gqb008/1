049ca427b49f3d768f54fefade479987e0299fec
tts <gqb008@yeah.net>
2017-02-26 18:31:19 +0800

    first

---
+++ src/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Welcome(string name,int ID=1)
+        {
+            return HtmlEncoder.Default.Encode($"hello:{name},id:{ID}");
+        }
+    }
+}
